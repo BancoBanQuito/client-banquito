1b9477af67edd7ec809f18eb9c33bd4cbbaa70ff
BRYAN ALEXANDER JIMENEZ DELGADO <bryanjd007@gmail.com>
2023-01-11 11:37:23 -0500

    Add Parameters ClientRepository

--- client/src/main/java/com/banquito/client/repository/ClientRepository.java
+++ client/src/main/java/com/banquito/client/repository/ClientRepository.java
@@ -1,10 +1,17 @@
 package com.banquito.client.repository;
 
-import com.banquito.client.model.Client;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
-@Repository
-public interface ClientRepository extends MongoRepository<Client, String>{
-    
+import org.springframework.data.repository.CrudRepository;
+
+import com.banquito.client.model.Client;
+
+public interface ClientRepository extends CrudRepository<Client, String>{
+
+    Client findByTypeIdentificationAndIdentification(String identificationType, String identification);
+
+    List<Client> findByLastnameOrderByNames(String lastname);
+    List<Client> findByLastnameLikeOrderByNames(String lastname);
+    List<Client> findByStatusOrderByNames(String status);
+    List<Client> findBySegmentOrderByNames(String segment);
 }
